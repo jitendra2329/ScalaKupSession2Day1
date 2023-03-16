5fce7c43a53de0e735a2ca9c95f25a1345d09f0c
Jitendra <jitendra.kumar@knoldus.com>
2023-03-16 23:54:58 +0530

    file modified

--- src/main/scala/com/knoldus/Functions.scala
+++ src/main/scala/com/knoldus/Functions.scala
@@ -1,11 +1,24 @@
 package com.knoldus
+object AdditionExceptions {
+    class IntOverflowException extends RuntimeException
+    class NegativeNumberException extends RuntimeException
+}
 
 object Functions extends App {
+    import AdditionExceptions._
     // 1.
     def addition: Function2[Int,Int,Int] = new Function2[Int, Int,Int] {
-      override def apply(firstNumber: Int, secondNumber: Int) = firstNumber + secondNumber
-    }
 
+        // this function will add only positive values. In case of negative values, it will throw NegativeNumberException
+        // is result is negative is will throw IntOverflowException
+
+      override def apply(firstNumber: Int, secondNumber: Int) = {
+          if (firstNumber < 0 || secondNumber < 0) throw new NegativeNumberException
+          val result = firstNumber + secondNumber
+          if (result < 0) throw new IntOverflowException
+          result
+        }
+    }
     // 2.
     def sumOfList: Function1[List[Int], Int] = new Function1[List[Int], Int] {
       override def apply(list: List[Int]): Int = list.sum
